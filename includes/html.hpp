0d0e3e398e47c5020fc8271ee7e91874c182870e
JuiceFV <alexcar19622000@gmail.com>
2019-11-23 13:12:02 +0300

    Error fixed

--- includes/html.hpp
+++ includes/html.hpp
@@ -1,117 +1,102 @@
 #pragma once
 
-#include <list>
-#include <unordered_map>
-#include <string>
 #include <fstream>
 #include <iostream>
+#include <list>
+#include <string>
+#include <unordered_map>
 
 void parser(std::string filename);
 
-struct Head
-{
-	std::unordered_map<std::string, std::string> meta;// <property-value, content-value>
+// the structure which shows the representation of the head-tag
+struct Head {
+  std::unordered_map<std::string, std::string>
+      meta;  // <property-value, content-value>
+  friend std::ostream& operator<<(std::ostream& out, const Head& head) {
+    for (auto m_property : head.meta) {
+      out << '[' << m_property.first << ", " << m_property.second << ']'
+          << std::endl;
+    }
+    return (out);
+  }
 };
 
-struct Body
-{
-	std::string title;
-	std::string passage;
+struct Body {
+  std::string title;
+  std::string passage;
 };
 
-class HTML
-{
-private:
-	Head head_tag;
-	Body body_tag;
-public:
-	HTML()
-	{
-		head_tag.meta = {'\0', '\0'};
-		body_tag.passage = '\0';
-		body_tag.title = '\0';
-	};
+class HTML {
+ private:
+  Head head_tag;
+  Body body_tag;
 
-	/*HTML(std::string filename)
-	{
-		*this = parser(filename);
-	};*/
+ public:
+  HTML(){};
 
-	void set_head_tag(const Head head)
-	{
-		head_tag = head;
-	}
+ /* HTML(std::string filename)
+  {
+          *this = parser(filename);
+  };*/
 
-	Head get_head_tag() const
-	{
-		return (head_tag);
-	}
+  void set_head_tag(const Head head) { head_tag = head; }
 
-	void set_body_tag(const Body body)
-	{
-		body_tag = body;
-	}
-	Body get_body_tag() const
-	{
-		return (body_tag);
-	}
+  Head get_head_tag() const { return (head_tag); }
+
+  void set_body_tag(const Body body) { body_tag = body; }
+  Body get_body_tag() const { return (body_tag); }
 };
 
-void parser(std::string filename)
-{
-	HTML res;
-	std::ifstream fin;
-	auto open = [&fin, &filename](){
-		fin.open(filename);
-		if (!fin.is_open())
-		{
-			std::cerr << "There is no file: " << filename;
-			exit(getchar());
-		}
-	};
-	open();
+void parser(std::string filename) {
+  HTML res;
+  std::ifstream fin;
 
-	auto get_head_info = [&fin](){
-		Head res; 
-		bool head_open = false;
-		std::string property[] = {"og:url", "og:site_name", "article:published_time", "og:title", "og:description"};
-		size_t content_start;
-		int i = 0;
+  // open the file
+  auto open = [&fin, &filename]() {
+    fin.open(filename);
+    if (!fin.is_open()) {
+      std::cerr << "There is no file: " << filename;
+      exit(getchar());
+    }
+  };
+  open();
 
-		while (!fin.eof())
-		{
-			std::string line;
-			std::getline(fin, line);
-			if (line.find("</head>") != std::string::npos)
-			{ 
-				head_open = false;
-				break;
-			}
-			if (head_open)
-			{ 
-				if ((content_start = line.find("content=")) != std::string::npos)
-					res.meta.insert({property[i++], line.substr(content_start + 8, line.size() - 2 - content_start - 8)});
-			}
-			if (line.find("<head>") != std::string::npos)
-				head_open = true;
-		}
-		return (res);
-	};
+  // here start the lambda wich acquire the info from tag head.
+  auto get_head_info = [&fin]() {
+    Head res;
+    bool head_open = false;
+    std::string property[] = {"og:url", "og:site_name",
+                              "article:published_time", "og:title",
+                              "og:description"};
+    size_t content_start;
+    int i = 0;
 
-	res.set_head_tag(get_head_info());
-	
-	auto get_body_info = [&fin]() {
-		Body res;
-		bool article_open = false;
-		while (!fin.eof())
-		{
-			std::string line;
-			std::getline(fin, line);
-			std::cout << line << std::endl;
-		}
-	};
-	get_body_info();
+    while (!fin.eof()) {
+      std::string line;
+      std::getline(fin, line);
+      if (line.find("</head>") != std::string::npos)
+        break;
+      if (head_open) {
+        if ((content_start = line.find("content=")) != std::string::npos)
+          res.meta.insert({property[i++],
+                           line.substr(content_start + 9,
+                                       line.size() - 2 - content_start - 10)});
+      }
+      if (line.find("<head>") != std::string::npos) head_open = true;
+    }
+    return (res);
+  };
+
+  res.set_head_tag(get_head_info());
+  //std::cout << res.get_head_tag();
+  auto get_body_info = [&fin]() {
+    Body res;
+    bool article_open = false;
+    while (!fin.eof()) {
+      std::string line;
+      std::getline(fin, line);
+      std::cout << line << std::endl;
+    }
+  };
+  get_body_info();
 }
-
-
-
